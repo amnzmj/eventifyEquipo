075c971a900ea649e9b0ce70ad07412b31ccdbec
amnzmj <amunoz31965@ucq.edu.mx>
2024-12-13 17:52:17 -0600

    auth error pequieño

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\TicketController;
+use Illuminate\Support\Facades\Auth;
+
 
 // Ruta principal
 // Route::get('public-events', function () {
